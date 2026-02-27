dfb91a276e6b74eedb98edb1f5c38e06f930a948
NoahYoung <noah227@foxmail.com>
2026-02-27 12:30:11 +0800

    chore: add semicolon at end of the import

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import {nodeResolve} from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import terser from "@rollup/plugin-terser"
-import json from "@rollup/plugin-json"
+import {nodeResolve} from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
 
 
 /**
